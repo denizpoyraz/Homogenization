6b6afd1a1cb818c73fd296ec4b852f305fb27a15
denizpoyraz <deniz.poyraz@meteo.be>
2020-09-23 16:33:44 +0200

    23-09-last

--- SondeCSV_reader.py
+++ SondeCSV_reader.py
@@ -7,26 +7,22 @@ import glob
 
 
 station = 'Praha'
-years = '1996'
 
-allFiles = glob.glob("/home/poyraden/Analysis/Homogenization_Analysis/Files/" + station + "/2020*csv")
+# allFiles = glob.glob("/home/poyraden/Analysis/Homogenization_Analysis/Files/" + station + "/19960219*csv")
+allFiles = glob.glob("/home/poyraden/Analysis/Homogenization_Analysis/Files/" + station + "/*.csv")
 
 list_data = []
 list_udata = []
-
 dfm = pd.DataFrame()
-
 fi = 0
 
 for filename in allFiles:
 
     print(filename)
-
     extcsv_to = load(filename)
 
     # access all tables
     tables = extcsv_to.sections.keys()
-    print(tables, len(tables))
 
     ## first copy the profile data and delete this from the keys to save the metadata. For Praha data it is 'PORFILE',
     # watch out that this naming may change from station to station
@@ -44,16 +40,39 @@ for filename in allFiles:
         dict = list(tables)[i]
         keys = extcsv_to.sections[dict].keys()
         ksize = len(keys)
+        if ksize == 1:
+            test = extcsv_to.sections[dict]['_raw']
+            test_df = pd.read_csv(StringIO(test))
+            tsize = len(test_df.columns)
+            for t in range(tsize):
+                cstr = test_df.columns.tolist()
+                if(len(test_df) > 0):
+                    dfm.at[fi, cstr[t]] = test_df.at[0,cstr[t]]
+                if(len(test_df) == 0): continue
+
         for j in range(1, ksize):
             mstr = dict
             kstr = list(keys)[j]
             if kstr == '_raw': kstr = ""
             astr = mstr + '_' + kstr
-            find = dfm.first_valid_index()
             # print(fi, " , " , mstr, " , ", kstr , " , ", extcsv_to.sections[dict][list(keys)[j]])
             dfm.at[fi, astr] = extcsv_to.sections[dict][list(keys)[j]]
 
-    #
+    #changes in the operation procedure
+    # check this information from the data-sheets provided by the station
+    # to be implemented by hand
+###
+    dfm.at[fi,'SondeTypeChange'] = False
+    dfm.at[fi,'SSTChange'] = False
+    dfm.at[fi,'BkgCurrentChange'] = 'iB2toiB0_2003'
+    dfm.at[fi,'BkgCurrentCorrectionChange'] = 'AltDeptoConstant_2003'
+    dfm.at[fi,'PumpTempLocationChange'] = 'BoxtoHole_19970131'
+    dfm.at[fi,'PumpFlowMeasChange'] = False
+    dfm.at[fi,'PumpFlowEfficiencyChange'] = False
+    dfm.at[fi,'TONChange'] = 'DobsontoBrewer_2000'
+    dfm.at[fi,'RSChange'] = True
+###
+
     dfprofile = StringIO(Profile)
     df = pd.read_csv(dfprofile)
     df['Date'] = dfm.at[fi, 'TIMESTAMP_Date']
@@ -61,7 +80,6 @@ for filename in allFiles:
     list_data.append(df)
 
     fi = fi + 1
-
     #
     # dfprofile_uncertainity = StringIO(Profile_uncertainity)
     # df_uncer = pd.read_csv(dfprofile_uncertainity)
@@ -70,6 +88,6 @@ for filename in allFiles:
 dff = pd.concat(list_data,ignore_index=True)
 # dfu = pd.concat(list_udata,ignore_index=True)
 
-dff.to_csv("/home/poyraden/Analysis/Homogenization_Analysis/Files/DF_" + station + "_1996.csv")
+dff.to_csv("/home/poyraden/Analysis/Homogenization_Analysis/Files/DF_" + station + "_All.csv")
 # dfu.to_csv("/home/poyraden/Analysis/Homogenization_Analysis/Files/DF_" + station + "_1996_unc.csv")
-dfm.to_csv("/home/poyraden/Analysis/Homogenization_Analysis/Files/DF_" + station + "_1996_metadata.csv")
+dfm.to_csv("/home/poyraden/Analysis/Homogenization_Analysis/Files/DF_" + station + "_All_metadata.csv")
